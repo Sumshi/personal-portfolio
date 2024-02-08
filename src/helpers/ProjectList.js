c03d1c27572848ec51ed4899b45cc8a33d7fc125
Sumshi <sumayasomow@gmail.com>
2024-02-08 14:41:09 +0300

    updating projects

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,32 @@
+// this is a separate file for all project lists
+// u can also put the same in projects.js filee refer to commenented sec
+import career from '../assets/career.PNG';
+import ecommerce from '../assets/ecommerce.PNG';
+import sunset from '../assets/sunset.PNG';
+import clock from '../assets/clock.PNG';
+
+export const ProjectList = [
+    {
+        name: "Careerlink",
+        image: career,
+        skills: "Html, Css, Javascript, Python(flask), MySql",
+    },
+
+    {
+        name: "Ecommerce website",
+        image: ecommerce,
+        skills: "Html, Css, Javascript",
+    },
+
+    {
+        name: "Sunset Bay Resort",
+        image: sunset,
+        skills: "Html, Css, Javascript",
+    },
+
+    {
+        name: "Clock",
+        image: clock,
+        skills: "Html, Css",
+    },
+]
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem';
+// import file where project list is defined
+// import { ProjectList } from "../helpers/ProjectList";
+import '../styles/Projects.css';
+import career from '../assets/career.PNG';
+import ecommerce from '../assets/ecommerce.PNG';
+import sunset from '../assets/sunset.PNG';
+import clock from '../assets/clock.PNG';
+
 
 function Projects() {
   return (
-    <div>
-    </div>
+    <div className='projects'>
+      <h1>My Projects</h1>
+      <div className='projectList'>
+        <ProjectItem name="Careerlink" image={career}/>
+        <ProjectItem name="Eccomerce website" image={ecommerce}/>
+        <ProjectItem name="Sunset Bay Resort" image={sunset}/>
+        <ProjectItem name="Clock" image={clock}/>
+
+        {/* {ProjectList.map(() => {
+          return <ProjectItem name={Projects.name} image={Projects.image} skills={Projects.skills} />
+        })} */}
+
+      </div>
+    </div> 
   );
 }
 
